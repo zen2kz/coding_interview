76bb8fd92f315cc93a61c7c9afab4c40d2f9e4e2
Gene S <yevgeniy.surtayev@gmail.com>
2022-11-25 06:53:10 -0700

    Cyclic Sort and Binary Tree - Scala

---
+++ scala/BinaryTree.scala
@@ -0,0 +1,68 @@
+import scala.collection.mutable.ArrayBuffer
+
+object BinaryTree {
+
+  case class Tree(value:Integer, left:Option[Tree], right:Option[Tree])
+
+  def breadthFirstLevels(head: Tree): Array[Array[Int]] = {
+    var queue = scala.collection.mutable.Queue[Tree](head)
+    val res = ArrayBuffer[Array[Int]]()
+    while (!queue.isEmpty) {
+        var level = Array[Int]()
+        val nextLevel = scala.collection.mutable.Queue[Tree]()
+        while (!queue.isEmpty) {
+            val curr = queue.dequeue()
+            level = level :+ curr.value
+            if (curr.left.isDefined) {
+                nextLevel.enqueue(curr.left.get)
+            }
+            if (curr.right.isDefined) {
+                nextLevel.enqueue(curr.right.get)
+            }
+        }
+        res += level
+        queue = nextLevel
+    } 
+    res.toArray
+  }
+
+  def buildBinaryTree1ToN(n: Int): Tree = {
+      var items = ArrayBuffer[Int]()
+      for (i<-1 to n) {
+        items += i
+      }
+      buildLevel(items.toArray,0).get
+  }
+
+  def printInOrder(node: Option[Tree]):Unit = {
+    if (node.isDefined) {
+        val currNode = node.get
+        printInOrder(currNode.left)
+        print(s"${currNode.value} ")
+        printInOrder(currNode.right)
+    }
+  }
+
+  def buildLevel(arr: Array[Int],  i:Int):Option[Tree] = {
+        var root:Option[Tree] = None
+        
+        // Base case for recursion
+        if (i < arr.length) {
+            val left = buildLevel(arr, 2 * i + 1);
+            val right = buildLevel(arr, 2 * i + 2);
+            
+            root = Some(Tree(arr(i), left, right))
+        }
+        root
+    }
+
+  //scalac BinaryTree.scala && scala BinaryTree
+  def main(args: Array[String])={
+        //val tree = Tree(1, Some(Tree(2, Some(Tree(4, None, None)), Some(Tree(5, None, None)))),Some(Tree(3, Some(Tree(6, None, None)), None)))
+
+        val tree = buildBinaryTree1ToN(6)
+        println(s"binary tree (in order) ${printInOrder(Some(tree))}")
+
+        println(s"binary tree levels are ${breadthFirstLevels(tree).map(_.mkString(","))mkString("|")} [1|2,3|4,5,6]")
+    }
+}
---
+++ scala/CyclicSort.scala
@@ -0,0 +1,60 @@
+import scala.util.control.Breaks._
+
+object CyclicSort {
+  // keyword: range from n to m, sort, scrambled
+
+  // sort array of scrambled 1..n in-place  
+  // time complexity is 0
+  def sortArray1toN(arr: Array[Int]): Array[Int] = {
+    var i = 0
+    while (i<arr.size) {
+        var curr = arr(i)
+        if (curr == i+1) {
+            i +=1
+        } else {
+            val stash = arr(curr-1)
+            arr(curr-1) = curr
+            arr(i) = stash
+        }
+    }
+
+    arr
+  }
+
+  def findMissingIndex(arr: Array[Int]): Int = {
+    var i = 0
+    while (i<arr.size) {
+        var curr = arr(i)
+        if (curr == i || curr>=arr.size) {
+            i +=1
+        } else {
+            val stash = arr(curr)
+            arr(curr) = curr
+            arr(i) = stash
+        }
+    }
+
+    var missedIndex = arr.size
+    breakable {
+        for (i<-0 until arr.size) {
+            if (arr(i)!=i) {
+                missedIndex = i 
+                break
+            }
+        }
+    }
+    missedIndex
+  }
+
+  //scalac CyclicSort.scala && scala CyclicSort
+  def main(args: Array[String])={
+        val array = Array(2,6,4,3,1,5)
+        println(s"sorted array is ${sortArray1toN(array).mkString(",")} [1..6]")
+
+        val array2 = Array(4,0,3,1)
+        println(s"missed index is ${findMissingIndex(array2)} [2]")
+
+        val array3 = Array(4,0,3,1,2)
+        println(s"missed index is ${findMissingIndex(array3)} [5]")
+    }
+}
